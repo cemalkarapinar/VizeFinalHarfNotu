e4d5f3d45142a60aaea20be1da5426e72dbc18e4
cemalkarapinar <87015331+cemalkarapinar@users.noreply.github.com>
2021-08-11 17:22:05 +0300

    Initial commit

---
+++ VizeFinalHarfNotu/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VizeFinalHarfNotu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int vize =Convert.ToInt32(textBox1.Text);
+            int final=Convert.ToInt32(textBox2.Text);
+            int ortalama;
+            ortalama = (vize * 40 / 100) + (final * 60 / 100);
+            if(ortalama >= 88)
+            {
+                label5.Text = "AA";
+                label6.Text = ortalama.ToString();
+            }
+            if (ortalama >= 81 & ortalama <=87)
+            {
+                label5.Text = "BA";
+                label6.Text = ortalama.ToString();
+            }
+            if (ortalama >= 74 & ortalama <= 80)
+            {
+                label5.Text = "BB";
+                label6.Text = ortalama.ToString();
+            }
+            if (ortalama >= 67 & ortalama <= 73)
+            {
+                label5.Text = "CB";
+                label6.Text = ortalama.ToString();
+            }
+            if (ortalama >=60 & ortalama <= 66)
+            {
+                label5.Text = "CC";
+                label6.Text = ortalama.ToString();
+            }
+            if (ortalama >= 53 & ortalama <= 59)
+            {
+                label5.Text = "DC";
+                label6.Text = ortalama.ToString();
+            }
+            if (ortalama >= 46 & ortalama <= 52)
+            {
+                label5.Text = "DD";
+                label6.Text = ortalama.ToString();
+            }
+            if (ortalama >= 39 & ortalama <= 45)
+            {
+                label5.Text = "FD";
+                label6.Text = ortalama.ToString();
+            }
+            if (ortalama >= 0 & ortalama <= 38)
+            {
+                label5.Text = "FF";
+                label6.Text = ortalama.ToString();
+            }
+
+
+        }
+    }
+}
---
+++ VizeFinalHarfNotu/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("VizeFinalHarfNotu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VizeFinalHarfNotu")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("7cff97c5-efd9-46b4-99e3-f3765b3cf23b")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
